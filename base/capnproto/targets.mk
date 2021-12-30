d6971ca13b3f46dc9cd5e8a6042fcf775187f577
Juan Cruz Viotti <jv@jviotti.com>
2021-12-29 22:05:53 -0400

    Add Cap'n Proto decode step
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- base/capnproto/targets.mk
+++ base/capnproto/targets.mk
@@ -1,3 +1,6 @@
 output/%/capnproto/output.bin: output/%/capnproto/encode.json benchmark/%/capnproto/schema.capnp
 	$(CAPNP) convert json:binary $(word 2,$^) Main < $(word 1,$^) > $@
 	xxd $@
+
+output/%/capnproto/decode.json: output/%/capnproto/output.bin benchmark/%/capnproto/schema.capnp
+	$(CAPNP) convert binary:json $(word 2,$^) Main < $< > $@
