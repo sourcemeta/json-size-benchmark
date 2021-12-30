cb94edb31d51526148b43a935c0c9466f60285db
Juan Cruz Viotti <jv@jviotti.com>
2021-12-30 15:57:57 -0400

    Use OUTPUT_DIRECTORY in Cap'n Proto targets
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- formats/capnproto/targets.mk
+++ formats/capnproto/targets.mk
@@ -1,6 +1,8 @@
-output/%/capnproto/output.bin: output/%/capnproto/input.json benchmark/%/capnproto/schema.capnp
+$(OUTPUT_DIRECTORY)/%/capnproto/output.bin: \
+	$(OUTPUT_DIRECTORY)/%/capnproto/input.json benchmark/%/capnproto/schema.capnp
 	$(CAPNP) convert json:binary $(word 2,$^) Main < $(word 1,$^) > $@
 	xxd $@
 
-output/%/capnproto/output.json: output/%/capnproto/output.bin benchmark/%/capnproto/schema.capnp
+$(OUTPUT_DIRECTORY)/%/capnproto/output.json: \
+	$(OUTPUT_DIRECTORY)/%/capnproto/output.bin benchmark/%/capnproto/schema.capnp
 	$(CAPNP) convert binary:json $(word 2,$^) Main < $< > $@
