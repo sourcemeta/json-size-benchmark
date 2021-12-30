613a6f2f58ae72c215bb6fb4e91b651ca7b445b1
Juan Cruz Viotti <jv@jviotti.com>
2021-12-30 15:58:22 -0400

    Remove xxd invocation
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- formats/capnproto/targets.mk
+++ formats/capnproto/targets.mk
@@ -1,7 +1,6 @@
 $(OUTPUT_DIRECTORY)/%/capnproto/output.bin: \
 	$(OUTPUT_DIRECTORY)/%/capnproto/input.json benchmark/%/capnproto/schema.capnp
 	$(CAPNP) convert json:binary $(word 2,$^) Main < $(word 1,$^) > $@
-	xxd $@
 
 $(OUTPUT_DIRECTORY)/%/capnproto/output.json: \
 	$(OUTPUT_DIRECTORY)/%/capnproto/output.bin benchmark/%/capnproto/schema.capnp
