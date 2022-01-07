9c02d73a9297b5362326d277d12bafab0285b0da
Juan Cruz Viotti <jv@jviotti.com>
2022-01-06 22:16:22 -0400

    Add color generation to charts
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- web/app.js
+++ web/app.js
@@ -1,5 +1,8 @@
 const Chart = require('chart.js')
+const autocolors = require('chartjs-plugin-autocolors')
 const data = JSON.parse(document.getElementById('data').textContent)
+
+Chart.register(autocolors)
 
 for (const entry of data) {
   const chart = new Chart(document.getElementById(`canvas-${entry.id}`), {
