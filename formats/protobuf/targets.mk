8c9261e4f5ed8dfaf7555111419cc4657995e1ee
Juan Cruz Viotti <jv@jviotti.com>
2022-01-08 15:14:29 -0400

    Use variables to refer to "env"
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- formats/protobuf/targets.mk
+++ formats/protobuf/targets.mk
@@ -6,13 +6,13 @@ $(OUTPUT)/documents/%/protobuf/output.bin: formats/protobuf/encode.py \
 	$(OUTPUT)/documents/%/protobuf/input.json $(OUTPUT)/documents/%/protobuf/schema_pb2.py \
 	benchmark/%/protobuf/run.py env \
 	| $(OUTPUT)/documents/%/protobuf
-	PYTHONPATH="$(dir $(word 3,$^)):$(dir $(word 4,$^))" ./env/bin/python $< $(word 2,$^) $@
+	PYTHONPATH="$(dir $(word 3,$^)):$(dir $(word 4,$^))" ./$(word 5,$^)/bin/python $< $(word 2,$^) $@
 
 $(OUTPUT)/documents/%/protobuf/output.json: formats/protobuf/decode.py \
 	$(OUTPUT)/documents/%/protobuf/output.bin $(OUTPUT)/documents/%/protobuf/schema_pb2.py \
 	benchmark/%/protobuf/run.py env \
 	| $(OUTPUT)/documents/%/protobuf
-	PYTHONPATH="$(dir $(word 3,$^)):$(dir $(word 4,$^))" ./env/bin/python $< $(word 2,$^) $@
+	PYTHONPATH="$(dir $(word 3,$^)):$(dir $(word 4,$^))" ./$(word 5,$^)/bin/python $< $(word 2,$^) $@
 
 $(OUTPUT)/documents/%/protobuf/VERSION: env scripts/version.py | $(OUTPUT)/documents/%/protobuf
 	$(PROTOC) --version > $@
