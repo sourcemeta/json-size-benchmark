c019b9c45412634d7712233f2a235392ef409dc8
Juan Cruz Viotti <jv@jviotti.com>
2022-01-08 15:19:23 -0400

    Use scoped python for CBOR
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- formats/cbor/targets.mk
+++ formats/cbor/targets.mk
@@ -1,10 +1,10 @@
-$(OUTPUT)/documents/%/cbor/output.bin: formats/cbor/encode.py $(OUTPUT)/documents/%/cbor/input.json \
+$(OUTPUT)/documents/%/cbor/output.bin: formats/cbor/encode.py $(OUTPUT)/documents/%/cbor/input.json env \
 	| $(OUTPUT)/documents/%/cbor
-	$(PYTHON) $< $(word 2,$^) $@
+	./$(word 3,$^)/bin/python $< $(word 2,$^) $@
 
-$(OUTPUT)/documents/%/cbor/output.json: formats/cbor/decode.py $(OUTPUT)/documents/%/cbor/output.bin \
+$(OUTPUT)/documents/%/cbor/output.json: formats/cbor/decode.py $(OUTPUT)/documents/%/cbor/output.bin env \
 	| $(OUTPUT)/documents/%/cbor
-	$(PYTHON) $< $(word 2,$^) > $@
+	./$(word 3,$^)/bin/python $< $(word 2,$^) > $@
 
-$(OUTPUT)/documents/%/cbor/VERSION: scripts/version.py | $(OUTPUT)/documents/%/cbor
-	$(PYTHON) $< cbor2 > $@
+$(OUTPUT)/documents/%/cbor/VERSION: env scripts/version.py | $(OUTPUT)/documents/%/cbor
+	./$</bin/python $(word 2,$^) cbor2 > $@
