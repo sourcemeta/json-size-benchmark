c456108ad03b44c6e239955496fb1991e606b1f7
Juan Cruz Viotti <jv@jviotti.com>
2022-01-08 17:45:50 -0400

    Make capnproto-packed use the same schema from capnproto
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/circleciblank/capnproto-packed/schema.capnp
+++
@@ -1,5 +0,0 @@
-@0xbca85c994f6e9ddc;
-
-struct Main {
-  version @0 :Float32;
-}
--- formats/capnproto-packed/targets.mk
+++ formats/capnproto-packed/targets.mk
@@ -1,10 +1,12 @@
+# Use the schema schema from capnproto
+
 $(OUTPUT)/documents/%/capnproto-packed/output.bin: \
-	$(OUTPUT)/documents/%/capnproto-packed/input.json benchmark/%/capnproto-packed/schema.capnp \
+	$(OUTPUT)/documents/%/capnproto-packed/input.json benchmark/%/capnproto/schema.capnp \
 	| $(OUTPUT)/documents/%/capnproto-packed
 	$(CAPNP) convert json:packed $(word 2,$^) Main < $(word 1,$^) > $@
 
 $(OUTPUT)/documents/%/capnproto-packed/output.json: \
-	$(OUTPUT)/documents/%/capnproto-packed/output.bin benchmark/%/capnproto-packed/schema.capnp \
+	$(OUTPUT)/documents/%/capnproto-packed/output.bin benchmark/%/capnproto/schema.capnp \
 	| $(OUTPUT)/documents/%/capnproto-packed
 	$(CAPNP) convert packed:json $(word 2,$^) Main < $< > $@
 
