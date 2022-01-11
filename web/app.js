75581468b2ad811dd273ecc153996d288a0f50d0
Juan Cruz Viotti <jv@jviotti.com>
2022-01-11 18:37:11 -0400

    Add colouring support again
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- web/app.js
+++ web/app.js
@@ -1,13 +1,31 @@
 const Chart = require('/usr/share/nodejs/chart.js')
 const data = JSON.parse(document.getElementById('data').textContent)
 
+// From Chart.js samples
+const chartColors = [
+  'rgb(255, 99, 132)',
+  'rgb(255, 159, 64)',
+  'rgb(255, 205, 86)',
+  'rgb(75, 192, 192)',
+  'rgb(54, 162, 235)',
+  'rgb(153, 102, 255)',
+  'rgb(201, 203, 207)'
+]
+
 for (const entry of data) {
   // eslint-disable-next-line no-new
   new Chart(document.getElementById(`canvas-${entry.id}`), {
     type: 'bar',
     data: {
       labels: entry.labels,
-      datasets: entry.datasets
+      datasets: entry.datasets.map((dataset, index) => {
+        return Object.assign({
+          backgroundColor:
+            Chart.helpers.color(chartColors[index]).alpha(0.8).rgbString(),
+          borderColor: chartColors[index],
+          borderWidth: 1
+        }, dataset)
+      })
     },
     options: {
       responsive: true,
