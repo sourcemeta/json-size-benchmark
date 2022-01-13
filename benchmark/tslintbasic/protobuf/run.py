9cfabc276ca5461372d5987d14991dabcb1a8b35
Juan Cruz Viotti <jv@jviotti.com>
2022-01-13 18:19:47 -0400

    Add tslintbasic
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ benchmark/tslintbasic/protobuf/run.py
@@ -0,0 +1,17 @@
+def encode(json, schema):
+    payload = schema.Main()
+    payload.rules.orderedImports.options.groupedImports = \
+        json['rules']['ordered-imports']['options']['grouped-imports']
+    return payload
+
+
+def decode(payload):
+    return {
+      'rules': {
+        'ordered-imports': {
+          'options': {
+            'grouped-imports': payload.rules.orderedImports.options.groupedImports
+          }
+        }
+      }
+    }
