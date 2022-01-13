eeea2a8977a012af7e51f80bb1607c0d076e3ce7
Juan Cruz Viotti <jv@jviotti.com>
2022-01-12 21:41:23 -0400

    Add commitlintbasic document
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ benchmark/commitlintbasic/thrift/run.py
@@ -0,0 +1,8 @@
+def encode(json, schema):
+    payload = schema.Main()
+    payload.defaultIgnores = json['defaultIgnores']
+    return payload
+
+
+def decode(payload):
+    return payload.__dict__
